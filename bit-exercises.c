75ebe4e9d25dbe32add1b0c081094a8567574df4
Gerrard Lindsay <gerrardalindsay@gmail.com>
2016-01-13 23:39:43 -0500

    Exercise 2-7

--- bit-exercises.c
+++ bit-exercises.c
@@ -36,11 +36,38 @@ void test_setbits(unsigned x, int y, int p, int n, unsigned expected_result)
   assert(result == expected_result);
 }
 
+/*
+Exercise 2-7. Write a function invert(x,p,n) that returns x with the n bits
+that begin at position p inverted (i.e., 1 changed into 0 and vice versa), leaving
+the others untouched.
+*/
+unsigned invert(unsigned x, int p, int n)
+{
+  unsigned inverted_mask = (~x & get_clear_mask(p - n + 1, n));
+
+  return (x & ~get_clear_mask(p - n + 1, n)) | inverted_mask;
+}
+
+void test_invert(unsigned x, int p, int n, unsigned expected_result) 
+{
+  unsigned result = invert(x, p, n);
+
+  printf("\nResults:\n\n");
+  printf("\t%-15s", "Expected:");
+  dump_bits(expected_result);
+  printf("\t%-15s", "Actual:");
+  dump_bits(result);
+  assert(result == expected_result);
+}
+
 int main() 
 {
-  test_setbits(87, 458, 7, 4, 167);
-  test_setbits(144, 7, 5, 3, 184);
+  /* test_setbits(87, 458, 7, 4, 167); */
+  /* test_setbits(144, 7, 5, 3, 184); */
 
-  /* printf("%ld\n", strtol("10111000", NULL, 2)); */
+  test_invert(170, 5, 4, 150);
+  test_invert(65280, 13, 2, 52992);
+
+  /* printf("%ld\n", strtol("1100111100000000", NULL, 2)); */
 }
 
