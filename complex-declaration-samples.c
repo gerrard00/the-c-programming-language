7d5bd6ea7cd1c52c7a3aa41ed8ba281f999f376f
Gerrard Lindsay <gerrardalindsay@gmail.com>
2016-03-08 13:05:39 -0500

    Break down one complex declaration manually.

---
+++ complex-declaration-samples.c
@@ -0,0 +1,35 @@
+/* function returning pointer to array[] of pointer to function returning char */
+char (*(*x())[])();
+
+///////////////
+
+char (*
+    (
+
+        // function returning a pointer to...
+        *x()
+
+    )[])();
+
+///////////////
+
+char (*function_returning_pointer_to
+      // array of...
+      []
+    )();
+
+///////////////
+
+char 
+  // pointer to function returning...
+  (*function_returning_pointer_to_array_of)();
+
+///////////////
+
+// char ;)
+char 
+  function_returning_pointer_to_array_of_pointer_to_function_returning; 
+
+///////////////
+
+function_returning_pointer_to_array_of_pointer_to_function_returning_char; 
